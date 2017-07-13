8150290381eaaf02ed97572222e157a3e471a389
blmy <mingyuan.l@hotmail.com>
2017-07-14 00:11:45 +0800

    First commit

---
+++ screens/FavouriteScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class FavouriteScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>FavouriteScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default FavouriteScreen;
---
+++ screens/MyOrderScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MyOrderScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MyOrderScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default MyOrderScreen;
