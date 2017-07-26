305fadaddee61c4be17e5d918a96dcba999b2209
blmy <mingyuan.l@hotmail.com>
2017-07-26 22:37:35 +0800

    amended store index.js typo

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import { CreateStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
