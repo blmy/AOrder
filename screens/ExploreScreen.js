1b9e081630772528d561224417e1cc1272be4e6b
blmy <mingyuan.l@hotmail.com>
2017-08-21 22:07:15 +0800

    added menu button in explore screen

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { MapView } from 'expo';
+import { Button } from 'react-native-elements';
 
 class ExploreScreen extends Component {
+  onComplete = () => {
+    this.props.navigation.navigate('menu');
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} />
-      </View>
+      <Button
+        title="Menu"
+        raised
+        buttonStyle={styles.buttonStyle}
+        onPress={this.onComplete}
+      />
     );
   }
 }
+
+const styles ={
+  buttonStyle: {
+    backgroundColor: '#0288D1',
+    marginTop: 15
+  }
+};
 
 export default ExploreScreen;
